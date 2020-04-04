4c5155acc08d5b55b9adc20a23bb2e3271e72b67
luotianqi777 <luotianqi777@gmail.com>
2020-04-04 22:39:12 +0800

    添加数据处理

--- Python/data.py
+++ Python/data.py
@@ -5,6 +5,7 @@
 """
 import matplotlib.pyplot as plt
 import requests
+import os
 import pandas as pd
 import json
 
@@ -23,20 +24,36 @@ class WebCrawler(object):
             print('request success')
         self.data = json.loads(r.text)['data']
         self.keys = keys
-        self.getData()
+        self.savePath = 'data.csv'
 
     def getData(self):
         pass
 
-    def run(self):
+    def saveData(self):
         result = []
         for key in self.keys:
-            frame = pd.DataFrame([unit[key] for unit in self.data])
+            frame = pd.DataFrame([unit[key] for unit in self.getData()])
             frame.columns = [key] if frame.shape[1] == 1 else [key + '_' + column for column in frame.columns]
             result.append(frame)
         data = pd.concat(result, axis=1)
-        data.plot()
+        data.to_csv(self.savePath, index=None)
+
+    def run(self):
+        if not os.path.exists(self.savePath):
+            self.saveData()
+        data = pd.read_csv(self.savePath)
+        data['today_storeConfirm'] = data['total_confirm']-data['total_dead']-data['total_heal']
+        data['date'] = pd.to_datetime(data['date'])
+        data.set_index('date', inplace=True)
+        print(data.describe())
+        print(data.info())
+        data[['today_storeConfirm', 'today_confirm', 'today_heal', 'today_dead', 'total_dead']].plot(marker='o', ms=3)
+        plt.legend(bbox_to_anchor=[1, 1])
+        plt.grid(axis='y')
+        plt.ylabel('people')
+        plt.box(False)
         plt.show()
+
 
 
 class Province(WebCrawler):
@@ -44,7 +61,7 @@ class Province(WebCrawler):
         super().__init__(keys=['name', 'total', 'today'])
 
     def getData(self):
-        self.data = self.data['areaTree'][2]['children']
+        return self.data['areaTree'][2]['children']
 
 
 class Country(WebCrawler):
@@ -52,19 +69,20 @@ class Country(WebCrawler):
         super().__init__(keys=['name', 'total', 'today'])
 
     def getData(self):
-        self.data = self.data['areaTree']
+        return self.data['areaTree']
 
 
 class DayList(WebCrawler):
     def __init__(self):
-        super().__init__(keys=['date', 'today'])
+        super().__init__(keys=['date', 'today', 'total'])
 
     def getData(self):
-        self.data = self.data['chinaDayList']
+        return self.data['chinaDayList']
 
 
 if __name__ == '__main__':
-    # bug = Province()
-    bug = DayList()
-    # bug = Country()
-    bug.run()
+    # crawler = Province()
+    crawler = DayList()
+    # crawler = Country()
+    crawler.run()
+
