286355746be2d5e35135bca3e7157e101aacbe8e
luotianqi777 <luotianqi777@gmail.com>
2020-04-10 18:27:41 +0800

    更新数据

--- Python/data.py
+++ Python/data.py
@@ -3,11 +3,14 @@
     time: 20.4.2
     get COVID-19 data by web crawler
 """
-import matplotlib.pyplot as plt
-import requests
 import os
-import pandas as pd
 import json
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import pyecharts.options as opts
+import pyecharts.charts as charts
+import pyecharts.faker as faker
 
 
 class WebCrawler(object):
@@ -17,14 +20,9 @@ class WebCrawler(object):
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
         }
         self.url = 'https://c.m.163.com/ug/api/wuhan/app/data/list-total'
-        r = requests.get(url=self.url, headers=self.headers)
-        if not r.status_code == 200:
-            print('request error')
-        else:
-            print('request success')
-        self.data = json.loads(r.text)['data']
         self.keys = keys
         self.savePath = 'data.csv'
+        self.data = []
         self.nameDict = {
             'date': '日期',
             'today_confirm': '今日确诊',
@@ -44,13 +42,21 @@ class WebCrawler(object):
         pass
 
     def saveData(self):
+        r = requests.get(url=self.url, headers=self.headers)
+        if not r.status_code == 200:
+            print('request error')
+        else:
+            print('request success')
+        self.data = json.loads(r.text)['data']
         result = []
         for key in self.keys:
             frame = pd.DataFrame([unit[key] for unit in self.getData()])
-            frame.columns = [key] if frame.shape[1] == 1 else [key + '_' + column for column in frame.columns]
+            frame.columns = [key] if frame.shape[1] == 1 else [
+                key + '_' + column for column in frame.columns]
             result.append(frame)
         data = pd.concat(result, axis=1)
-        data['today_storeConfirm'] = data['total_confirm'] - data['total_dead'] - data['total_heal']
+        data['today_storeConfirm'] = data['total_confirm'] - \
+            data['total_dead'] - data['total_heal']
         data['date'] = pd.to_datetime(data['date'])
         data.set_index('date', inplace=True)
         data.to_csv(self.savePath)
@@ -61,9 +67,12 @@ class WebCrawler(object):
         data = pd.read_csv(self.savePath)
         print(data.describe())
         print(data.info())
-        plot_data = data[['today_storeConfirm', 'today_confirm', 'today_heal', 'today_dead', 'total_dead']]
-        # my system not supply chinese
-        # replace columns name from english to chinese, rule is nameDict
+        plot_data = data[['today_storeConfirm', 'today_confirm',
+                          'today_heal', 'today_dead', 'total_dead']]
+        '''
+            my system not supply chinese
+            replace columns name from english to chinese, rule is nameDict
+        '''
         # plot_data.rename(columns=self.nameDict, inplace=True)
         plot_data.plot(marker='o', ms=3)
         plt.legend(bbox_to_anchor=[1, 1])
@@ -101,4 +110,5 @@ if __name__ == '__main__':
     # crawler = Province()
     crawler = DayList()
     # crawler = Country()
+    # crawler.saveData()
     crawler.run()
